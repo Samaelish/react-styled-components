f950ccab45269184c2f00fbfac4426bda6a97d12
Samaelish <semnd007@gmail.com>
2022-03-29 16:54:50 +0300

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Title from './components/Title'
+import Flex from './components/Flex'
+import Console from './components/Console';
+import Button from './components/Button';
+
+const AppWrapper = styled.div`
+width: 100%;
+min-height: 100vh;
+padding: 2rem;
+background: #000;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppWrapper className="App">
+            <Flex justify="center">
+                <Title>Console cmd 2022. Samaelish</Title>
+            </Flex>
+            <Flex direction="column" margin={'0.625rem 0'}>
+                <Console />
+                <Button color='green' align="flex-end">Отправить</Button>
+            </Flex>
+        </AppWrapper>
+    );
 }
 
 export default App;
